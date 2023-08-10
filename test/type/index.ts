20d158b5a7ccedfde9f6ef206334aef2797071d9
Sam A. Horvath-Hunt <hello@samhh.com>
2023-08-10 18:34:50 +0100

    Add type tests

--- test/type/index.ts
+++ test/type/index.ts
@@ -5,10 +5,16 @@ import {
   getCodecFromSerialized,
   getCodecFromMappedNullaryTag,
   getCodecFromNullaryTag,
+  getUntaggedCodec,
+  getInternallyTaggedCodec,
+  getExternallyTaggedCodec,
+  getAdjacentlyTaggedCodec,
+  nullary,
 } from "../../src/index"
 import * as t from "io-ts"
 import { constant } from "fp-ts/function"
 import * as O from "fp-ts/Option"
+import { NumberFromString } from "io-ts-types"
 
 type A = Sum.Member<"A1"> | Sum.Member<"A2", number>
 const A = Sum.create<A>()
@@ -37,3 +43,25 @@ getCodecFromNullaryTag(B)(["B1"]) // $ExpectError
 getCodecFromNullaryTag(B)(["B2"]) // $ExpectError
 getCodecFromNullaryTag(B)(["B1", "B1"]) // $ExpectError
 getCodecFromNullaryTag(B)(["B1", "B2"]) // $ExpectType Type<B, string, unknown>
+
+type C = Sum.Member<"C1"> | Sum.Member<"C2", { readonly k: number }>
+const C = Sum.create<C>()
+
+const c1 = nullary
+const c2 = t.strict({ k: NumberFromString })
+
+getUntaggedCodec(C)({ C2: c2, C1: c1 }) // $ExpectType Type<C, Record<string, unknown> | { k: string; } | null | undefined, unknown>
+getUntaggedCodec(C)({ C1: c1 }) // $ExpectError
+getUntaggedCodec(C)({ C2: c2 }) // $ExpectError
+
+getInternallyTaggedCodec("tag")(C)({ C2: c2, C1: c1 }) // $ExpectType Type<C, (Record<"tag", "C1"> & Record<string, unknown>) | (Record<"tag", "C2"> & { k: string; }), unknown>
+getInternallyTaggedCodec("tag")(C)({ C1: c1 }) // $ExpectError
+getInternallyTaggedCodec("tag")(C)({ C2: c2 }) // $ExpectError
+
+getExternallyTaggedCodec(C)({ C2: c2, C1: c1 }) // $ExpectType Type<C, Record<"C1", Record<string, unknown> | null | undefined> | Record<"C2", { k: string; }>, unknown>
+getExternallyTaggedCodec(C)({ C1: c1 }) // $ExpectError
+getExternallyTaggedCodec(C)({ C2: c2 }) // $ExpectError
+
+getAdjacentlyTaggedCodec("tag")("val")(C)({ C2: c2, C1: c1 }) // $ExpectType Type<C, (Record<"tag", "C1"> & Record<"val", Record<string, unknown> | null | undefined>) | (Record<"tag", "C2"> & Record<"val", { k: string; }>), unknown>
+getAdjacentlyTaggedCodec("tag")("val")(C)({ C1: c1 }) // $ExpectError
+getAdjacentlyTaggedCodec("tag")("val")(C)({ C2: c2 }) // $ExpectError
